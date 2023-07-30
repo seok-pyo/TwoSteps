deb39a40c1dfc57643088aa2a9b42aee65ced2b5
SP-hong <hongseokpyou@gmail.com>
2023-07-30 21:31:29 +0900

    Set object

---
+++ Set_Object_0730.js
@@ -0,0 +1,9 @@
+// Set 은 JavaScript 에서 제공하는 자료 구조 중 하나로, 값들의 컬렉션(collection)을 저장하는 객체이다.
+// Set은 중복되지 않은 유일한 값을 저장하며, 순서를 유지한다.
+
+// add(value) : Set에 값을 추가하낟. 이미 존재하는 경우 추가되지 않음
+// delete(value) : Set에서 값을 제거한다. 값이 제거되면 true, 아니면 false를 리턴
+// has(value) : Set에 특정 값을 포함하는지 여부를 확인
+// clear()
+// size
+// 값으로 접근

20e6bba15c6e22fcd3b3a4f7349fcbfe1c271a39
seok-pyo <hongseokpyou@gmail.com>
2023-10-03 15:39:25 +0900

    디렉토리 정리

---
+++ jest/fn.js
@@ -0,0 +1,11 @@
+const fn = {
+  add: (num1, num2) => num1 + num2,
+  getName: (callback) => {
+    const name = "Mike";
+    setTimeout(() => {
+      callback(name);
+    }, 3000);
+  },
+};
+
+module.exports = fn;

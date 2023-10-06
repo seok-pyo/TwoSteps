7dae1e90f42ec8811544a63d3049fd106fc8cf7c
seok-pyo <hongseokpyou@gmail.com>
2023-10-06 14:39:35 +0900

    class_inheritance_1

---
+++ CoreJavaScript/7.클래스(class)/class_Inheritance.js
@@ -0,0 +1,27 @@
+function Person(name, age) {
+  this.name = name || "이름없음";
+  this.age = age || "나이모름";
+}
+
+Person.prototype.getName = function () {
+  return this.name;
+};
+
+Person.prototype.getAge = function () {
+  return this.age;
+};
+
+function Employee(name, age, position) {
+  this.name = name || "이름없음";
+  this.age = age || "나이모름";
+  this.position = position || "직책모름";
+}
+
+Employee.protytype = new Person();
+Employee.prototype.constructor = Employee;
+Employee.prototype.getPosition = function () {
+  return this.position;
+};
+
+var sp = new Employee("sp", 30, "CEO");
+console.log(sp);

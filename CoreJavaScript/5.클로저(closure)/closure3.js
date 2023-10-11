1ea7a739c78db68c0373e53b306fc707f93b06e5
seok-pyo <hongseokpyou@gmail.com>
2023-10-11 14:56:05 +0900

    클로져 사례 스터디_2

--- CoreJavaScript/5.클로저(closure)/closure3.js
+++ CoreJavaScript/5.클로저(closure)/closure3.js
@@ -8,3 +8,14 @@ var outer = function () {
 var outer2 = outer();
 console.log(outer2());
 console.log(outer2());
+
+// 클로져가 아닌 경우
+var outer = function () {
+  var a = 1;
+  var inner = function () {
+    return ++a;
+  };
+  return inner();
+};
+var outer2 = outer();
+console.log(outer2());

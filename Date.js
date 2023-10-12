a5fb9e8e6ceebb9adce5e62d5e52ceff78526362
seok-pyo <hongseokpyou@gmail.com>
2023-10-12 20:50:57 +0900

    시간 계산 메서드

--- Date.js
+++ Date.js
@@ -6,3 +6,15 @@ const time =
 
 // 타임스탬프 얻기
 Date.now();
+
+// 초 얻기
+let t1 = Date.now();
+let t2 = Date.now();
+// 시간 계산 메서드
+function ts(t1, t2) {
+  let sec = parseInt((t2 - t1) / 1000);
+  let hour = parseInt(sec / 3600);
+  let min = parseInt((sec % 3600) / 60);
+  let fsec = (sec % 3600) % 60;
+  return `${hour}시간, ${min}분, ${fsec}초`;
+}

84b3eb79bfa744d545930ddd4f91752ed7ed0e92
seok-pyo <hongseokpyou@gmail.com>
2023-10-12 17:25:59 +0900

    타임스탬프 연습

---
+++ Date.js
@@ -0,0 +1,8 @@
+let now = new Date();
+
+const year = now.getFullYear() + " - " + now.getMonth() + " - " + now.getDay();
+const time =
+  now.getHours() + " : " + now.getMinutes() + " : " + now.getSeconds();
+
+// 타임스탬프 얻기
+Date.now();
